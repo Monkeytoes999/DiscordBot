0d8cf84cd7a0aeba9f8ab650533a52d346524636
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-02 19:02:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -489,7 +489,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		case 'test':
 			bot.sendMessage({
 				to: channelID,
-				message: arrayTest.length + ' ' + arrayTest[1].length + ' ' + arraytest[0].length
+				message: arrayTest.length + ' ' + arrayTest[1].length + ' ' + arrayTest[0].length
 			});
 			break;
 		case 'getChannelID':
