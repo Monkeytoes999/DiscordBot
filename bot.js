1d702b18ebbf6af5fac7b061dd3e614abce9f15b
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-01 21:13:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,6 +52,8 @@ var scDayChange = true;
 var scDay = 'oof';
 var remdSent = false;
 var dayay;
+var allstar = ['**All Star by Smash Mouth**', 'Somebody once told me the world is gonna roll me', 'I ain\'t the sharpest tool in the shed', 'She was looking kind of dumb with her finger and her thumb', 'In the shape of an "L" on her forehead', 'Well the years start coming and they don\'t stop coming', 'Fed to the rules and I hit the ground running', 'Didn\'t make sense not to live for fun', 'Your brain gets smart but your head gets dumb', 'So much to do, so much to see', 'So what\'s wrong with taking the back streets?', 'You\'ll never know if you don\'t go', 'You\'ll never shine if you don\'t glow', 'Hey now, you\'re an all-star, get your game on, go play', 'Hey now, you\'re a rock star, get the show on, get paid', 'And all that glitters is gold', 'Only shooting stars break the mold', 'It\'s a cool place and they say it gets colder', 'You\'re bundled up now, wait till you get older', 'But the meteor men beg to differ', 'Judging by the hole in the satellite picture', 'The ice we skate is getting pretty thin', 'The water\'s getting warm so you might as well swim', 'My world\'s on fire, how about yours?', 'That\'s the way I like it and I never get bored', 'Hey now, you\'re an all-star, get your game on, go play, 'Hey now, you\'re a rock star, get the show on, get paid', 'All that glitters is gold', 'Only shooting stars break the mold', 'Hey now, you\'re an all-star, get your game on, go play', 'Hey now, you\'re a rock star, get the show, on get paid', 'And all that glitters is gold', 'Only shooting stars', 'Somebody once asked could I spare some change for gas?', 'I need to get myself away from this place', 'I said yep what a concept', 'I could use a little fuel myself', 'And we could all use a little change', 'Well, the years start coming and they don\'t stop coming', 'Fed to the rules and I hit the ground running', 'Didn\'t make sense not to live for fun', 'Your brain gets smart but your head gets dumb', 'So much to do, so much to see', 'So what\'s wrong with taking the back streets?', 'You\'ll never know if you don\'t go (go!)', 'You\'ll never shine if you don\'t glow', 'Hey now, you\'re an all-star, get your game on, go play', 'Hey now, you\'re a rock star, get the show on, get paid', 'And all that glitters is gold', 'Only shooting stars break the mold', 'And all that glitters is gold', 'Only shooting stars break the mold'];
+var songs = [allstar, allstar];
 
 //team blue 499003285106196480
 //team red 499003389955407872
@@ -287,7 +289,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	}
        	
 	if (cussmessage.includes('OWO')){
-		if (serverID != 500864200378155008 && channelID != 501934275860496395){
+		if (serverID != 500864200378155008 && serverID != 505565358560772096 && channelID != 501934275860496395){
 	    		bot.sendMessage({
 				to: channelID,
 				message: 'What\'s this?'
@@ -469,6 +471,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				message: '<a:affa:502663197808132116>'
 			});
 			break;
+		case 'music':
+			let song2play = songs[Math.floor(Math.random() * 1)];
+			let line2play = 0;
+			for (line2play = 0; line2play < song2play.length; line2play++){
+				setTimeout(() => {
+   					 bot.sendMessage({
+						 to: channelID,
+						 message: song2play[line2play]
+					 });
+				}, 1000);
+			}
 		case 'test':
 			let oragne = test[0];
 			let testmessage123 = '';
