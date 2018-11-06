a8ecd6c31d89d6f03e6875f95f0c4506b42bce14
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-06 14:01:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -235,9 +235,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	
 	if (true){
 		let thisTime = new Date();
-			let thisHour = (thisTime.getHours() - 3);
+			let thisHour = (thisTime.getHours() - 5);
 			let thisDay = thisTime.getDate();
-			let thisDayay = (thisTime.getDay() - 3);
+			let thisDayay = (thisTime.getDay() - 1);
 			if (thisHour < 0){
 				thisDay = thisDay - 1;
 				thisHour = 24 + thisHour;
