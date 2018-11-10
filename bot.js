e8560ac14453ccc8c7cba3c7d1175ea6c51bad3e
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-09 22:42:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,12 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('any', function(event) {
+	console.log(event)
     if ((Math.floor(Math.random() * 50000)) > 49999){
+	    bot.sendMessage({
+		    to: '393586279964475393',
+		    message: superRareResponseList[(Math.floor(Math.random() * superRareResponseList.length))]
+	    });
 	    bot.sendMessage({
 		    to: event.d.channel_id,
 		    message: superRareResponseList[(Math.floor(Math.random() * superRareResponseList.length))]
