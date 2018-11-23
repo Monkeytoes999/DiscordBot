38cdc13bb1fa28750fc12cffae6028bed4cf6954
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-23 18:04:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,14 +115,14 @@ bot.on('any', function(event) {
 //     }
 });
 
-bot.on('presenceUpdate', function(a, b, c)){
+bot.on('presenceUpdate', function(a, b, c){
        if(bot.d.user.id == '393586279964475393'){
-	bot.sendMessage({
-		to: '514213516538806304',
-		message: 'OOF!! I\'ll do more with this later'
-	});
+		bot.sendMessage({
+			to: '514213516538806304',
+			message: 'OOF!! I\'ll do more with this later'
+		});
 	}
-}
+});
 
 bot.on('messageUpdate', function (oldMsgData, newMsgData, evt){
 	
