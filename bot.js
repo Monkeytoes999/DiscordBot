d20bf68076edaac0c6b11af342e44aac5f011cc6
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-23 23:47:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,9 +116,6 @@ bot.on('any', function(event) {
 });
 
 bot.on('presenceUpdate', function(event){
-	console.log(a)
-	console.log(b)
-	console.log(c)
        if(event.d.user.id == '393586279964475393'){
 		bot.sendMessage({
 			to: '514213516538806304',
