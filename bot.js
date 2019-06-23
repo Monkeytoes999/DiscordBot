26b9469f4fc68b8cdbcf39fdd8443ab9392f0a4e
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2019-06-23 13:36:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,7 +166,7 @@ bot.on('guildMemberAdd', function(member, evt){
 });
 
 bot.on('guildCreate', function(server){
-	dtb.query('SELECT cursedefault FROM servers WHERE id = ' + server.id, function(e, r){
+	dtb.query('SELECT cursedefault FROM servers WHERE id = \'' + server.id + '\'', function(e, r){
 		if (r != undefined){
 			if (r.rows[0] == undefined){
 				dtb.query('INSERT INTO servers(id, cursedefault) VALUES (' + server.id + ', false)');
@@ -271,7 +271,7 @@ bot.on('messageUpdate', function (oldMsgData, newMsgData, evt){
 		}
 	}
 
-		dtb.query('SELECT cursedefault FROM servers WHERE id = ' + serverID, function(e, r){
+		dtb.query('SELECT cursedefault FROM servers WHERE id = \'' + server.id + '\'', function(e, r){
 			if (r != undefined){
 				if (r.rows[0] != undefined){
 					allowCuss = r.rows[0];
@@ -590,7 +590,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		}
 	}
 	
-		dtb.query('SELECT cursedefault FROM servers WHERE id = ' + serverID, function(e, r){
+		dtb.query('SELECT cursedefault FROM servers WHERE id = \'' + server.id + '\'', function(e, r){
 			if (r != undefined){
 				if (r.rows[0] != undefined){
 					allowCuss = r.rows[0];
@@ -932,7 +932,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				break;
 			//Displays a random video for you
 			case 'toggleCuss':
-				dtb.query('SELECT cursedefault FROM servers WHERE id = ' + serverID, function(e, r){
+				dtb.query('SELECT cursedefault FROM servers WHERE id = \'' + server.id + '\'', function(e, r){
 					if (r != undefined){
 						if (r.rows[0] != undefined){
 							dtb.query('UPDATE servers SET cursedefault ' + !(r.rows[0]) + ' WHERE id = \'' + serverID + '\'');
@@ -944,12 +944,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 							});
 						}
 					}
-					console.log(e)
 				});
 				commRand = true;
 				break;
 			case 'updateVersion':
-				dtb.query('SELECT cursedefault FROM servers WHERE id = ' + server.id, function(e, r){
+				dtb.query('SELECT cursedefault FROM servers WHERE id = \'' + server.id + '\'', function(e, r){
 					if (r != undefined){
 						if (r.rows[0] == undefined){
 							dtb.query('INSERT INTO servers(id, cursedefault) VALUES (' + server.id + ', false)');
