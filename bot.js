07dded43722d5c49ea599f0238e1659666e8a01d
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2020-04-20 18:02:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -151,7 +151,7 @@ bot.on('disconnect', function(erMsg, code) {
     console.log('----- Bot disconnected from Discord with code', code, 'for reason:', erMsg, '-----');
 	bot.sendMessage({
 		to: '701915050688118804',
-		message: '----- Bot disconnected from Discord with code', code, 'for reason:', erMsg, '-----'
+		message: '----- Bot disconnected from Discord with code' + code + 'for reason:' + erMsg + '-----'
 	});
 	if (code != 1000){
 		bot.sendMessage({
