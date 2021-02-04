93a76b16d0d8a96c794c1ccab2afdda76b093fc6
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2021-02-04 11:34:13 -0500

    Update Eris.js

--- Eris.js
+++ Eris.js
@@ -562,13 +562,15 @@ bot.on('messageCreate', (msg) => {
 									topRoleID = bot.guilds.find(function(obj){return obj.id == serverID}).roles.find(function(obj){return obj.id == member.roles[iooof]}).id
 								}
 							}
-							console.log(message.length)
 							if (message.length > 32 && message.includes('<@&')){
 								if (topRole != 0 && bot.guilds.find(function(obj){return obj.id == serverID}).roles.find(function(obj){return obj.id == message.substring(message.indexOf('&') + 1, message.indexOf('&') + 19)}) != undefined){
 								    if (bot.guilds.find(function(obj){return obj.id == serverID}).roles.find(function(obj){return obj.id == message.substring(message.indexOf('&') + 1, message.indexOf('&') + 19)}).position < topRole && (bot.guilds.find(function(obj){return obj.id == serverID}).roles.find(function(obj){return obj.id == topRoleID}).permissions.has("administrator") || checkPerms((bot.guilds.find(function(obj){return obj.id == serverID}).roles.find(function(obj){return obj.id == topRoleID}).permissions.allow), 268435456, 1073741824))){
 										let rcRID = message.substring(message.indexOf('&') + 1, message.indexOf('&') + 19);
 										let rcCMD = message.substring(32)
-										console.log(rcRID, rcCMD)
+										dtb.query('SELECT command FROM rccm WHERE serverid = ' + serverID + ' and roleid = ' + rcRID, function(qerr, qres){
+											if (qerr) throw qerr;
+											console.log(qres)
+										});
 										dtb.query('INSERT INTO rccm(command, roleid, serverid) VALUES (\'' + rcCMD + '\', ' + rcRID + ', ' + serverID + ')', function(err, res){
 											if (err) throw err;
 											bot.createMessage(channelID, "Your command has been created.");
