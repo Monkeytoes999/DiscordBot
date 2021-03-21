ce0094339fddb2377910c74691989e04c96e698e
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2021-03-21 16:15:11 -0400

    Update Eris.js

--- Eris.js
+++ Eris.js
@@ -290,7 +290,7 @@ bot.on('messageCreate', (msg) => {
 		//Sets variables for time/date
 		let thisTime = new Date();
 			let thisMinute = thisTime.getMinutes();
-			let thisHour = (thisTime.getHours() - 6);
+			let thisHour = (thisTime.getHours() - 8);
 			let thisDay = thisTime.getDate();
 			let thisDayay = (thisTime.getDay() - 1);
 			if (thisHour < 0){
